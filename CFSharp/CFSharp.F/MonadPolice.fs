d35b7e6a1184262c0ec490eeec40bf454ba34cc3
David Tchepak <dave@davesquared.net>
2013-05-15 17:36:05 +1000

    removing explicit fields from f# class example

--- CFSharp/CFSharp.F/MonadPolice.fs
+++ CFSharp/CFSharp.F/MonadPolice.fs
@@ -10,21 +10,19 @@ type ImDave =
     abstract RecentRamblings : unit -> string seq // or Seq<string>
     
 type MonadPolice(dave : ImDave, email : IEmailGateway) = 
-    let _dave = dave
-    let _email = email
+    //let _dave = dave
+    //let _email = email
     member this.Surveil() = 
-        let overheard = _dave.RecentRamblings()
+        let overheard = dave.RecentRamblings()
         let zealotTalk = 
           overheard.Where(contains "monad")
         for outburstOfZealotry in zealotTalk do
-            _email.Send "xerxesb" "the monad police" 
+            email.Send "xerxesb" "the monad police" 
                         "Lack of pragmatism detected" outburstOfZealotry
 
 type MonadPolice_MoreFSharpish(dave : ImDave, email : IEmailGateway) = 
-    let _dave = dave
-    let _email = email
-    let sendOutburst = _email.Send "xerxesb" "the monad police" "Lack of pragmatism detected"
+    let sendOutburst = email.Send "xerxesb" "the monad police" "Lack of pragmatism detected"
     member this.Surveil() = 
-        _dave.RecentRamblings()
+        dave.RecentRamblings()
             |> Seq.filter (contains "monad")
             |> Seq.iter sendOutburst
