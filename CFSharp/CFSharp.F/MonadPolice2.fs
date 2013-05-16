c207418d544ced94fbef1aae977e32745738d603
David Tchepak <dave@davesquared.net>
2013-05-17 00:22:40 +1000

    record examples

--- CFSharp/CFSharp.F/MonadPolice2.fs
+++ CFSharp/CFSharp.F/MonadPolice2.fs
@@ -9,11 +9,20 @@ type TipOff =
     ; subject : string
     ; msg : string
     }
+    static member standard = { sendTo="xerxesb"; from="the monad police"; 
+                               subject="Lack of pragmatism detected"; msg="" }
+
+let tipOffsFor messages =
+    Seq.map (fun s -> { TipOff.standard with msg=s }) messages
+
+let sendVia (email : IEmailGateway) tip =
+    email.Send tip.sendTo tip.from tip.subject tip.msg
+
+let standardTipOff = { sendTo="xerxesb"; from="the monad police"; 
+                       subject="Lack of pragmatism detected"; msg="" }
 
 let surveil (talk : string seq) =
     let isZealotry = contains "monad"
-    let standardTipOff = { sendTo="xerxesb"; from="the monad police"; 
-                           subject="Lack of pragmatism detected"; msg="" }
     talk
         |> Seq.filter isZealotry
         |> Seq.map (fun s -> { standardTipOff with msg=s })
